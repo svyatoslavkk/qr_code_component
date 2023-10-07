919b2f348bfd8b85c655db28ed8125296ee6a3d4
svyatoslavkk <sv.krivonos1@gmail.com>
2023-10-07 10:20:46 +0300

    reports have been corrected

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className='content'>
-      <img src="/images/image-qr-code.png" className='code-img' />
-      <h2 className='title'>Improve your front-end skills by building projects</h2>
-      <p className='info'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
-    </div>
+    <main>
+      <div className='content'>
+        <img src="/images/image-qr-code.png" className='code-img' alt="Code Image" />
+        <h1 className='title'>Improve your front-end skills by building projects</h1>
+        <p className='info'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+      </div>
+    </main>
   );
 }
 
